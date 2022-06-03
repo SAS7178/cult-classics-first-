113e86ffcbc747cf806f37ca1bff04ba36e28b54
Christian Suriano <cratea113@gmail.com>
2022-06-03 10:55:19 -0500

    the best pet function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('Trees')
+
+const teamPets = () => {
+  const pets =  console.log("Stephen: dog, Wilson: skink, Robert: Ash & Brisco, Christian: Buster ")
+    return pets
+}
+
+const ourPets = teamPets()
+
+console.log(ourPets)
